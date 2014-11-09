63a2d08232591424f10a37e5b33d422077e52055
c2y3dc <ervinchow@gmail.com>
2014-11-08 17:33:41 -0800

    completed

--- watchout.js
+++ watchout.js
@@ -1,27 +1,32 @@
 // start slingin' some d3 here.
+
+
+
 var gameOptions = {
-  height: 450,
-  width: 700,
+  height: window.innerHeight,
+  width: window.innerWidth,
   numberOfEnemies: 30,
   padding: 20
 };
 
 var gameStats = {
   score: 0,
-  bestScore: 0
+  bestScore: 0,
+  collisions: 0
 }
 
 var axez = {
   x: d3.scale.linear()
       .domain([0, 100])
-      .range([0, gameOptions.width]),
+      .range([0, gameOptions.width*0.8]),
   y: d3.scale.linear()
       .domain([0, 100])
-      .range([0, gameOptions.height])
+      .range([0, gameOptions.height*0.9])
   };
 var gameBoard = d3.select("body").append("svg")
             .attr("width", gameOptions.width)
-            .attr("height", gameOptions.height);
+            .attr("height", gameOptions.height)
+            .attr('class','background');
 
 
 var drag = d3.behavior.drag()
@@ -31,17 +36,17 @@ var drag = d3.behavior.drag()
              .on('dragend', function() { player.style('fill', 'crimson'); });
 
 var player = gameBoard.selectAll('.player')
-                .data([{ x: (gameOptions.width / 2), y: (gameOptions.height / 2)}])
+                .data([{ x: (gameOptions.width / 2), y: (gameOptions.height / 4)}])
                 .enter()
                 .append('svg:image')
-                .attr('xlink:href', "icon_63757.svg")
+                .attr('xlink:href', "images/sun.svg")
                 .attr('class', 'player')
-                .attr('width', '30')
-                .attr('height', '30')
+                .attr('width', '80')
+                .attr('height', '80')
                 // .style({'top': function(d){ return d.y + 'px'},'left': function(d) { return d.x  + 'px'; }})
                 .attr('x', function(d) { return d.x; })
                 .attr('y', function(d) { return d.y; })
-                // .attr('r', function(d) { return d.r; })
+                .attr('r', function(d) { return d.r; })
                 .call(drag)
                 // .style('fill', 'crimson');
 
@@ -65,7 +70,7 @@ var ran = false;
 
 
 var renderEnemies = function(enemy_data){
-  var enemies = gameBoard.selectAll("circle.enemy").data(enemy_data, function(d){
+  var enemies = gameBoard.selectAll("image.enemy").data(enemy_data, function(d){
 
     return d.id
   })
@@ -85,40 +90,49 @@ var renderEnemies = function(enemy_data){
         }
 
         enemy = d3.select(this);
-        distanceBetweenx = Math.abs(player.attr("cx") - enemy.attr("cx"))
-        distanceBetweeny = Math.abs(player.attr("cy") - enemy.attr("cy"))
+        distanceBetweenx = Math.abs(player.attr("x") - enemy.attr("x"))
+        distanceBetweeny = Math.abs(player.attr("y") - enemy.attr("y"))
 
         if (distanceBetweenx<10 && distanceBetweeny<10){
           gameStats.score = 0;
+          gameStats.collisions++;
+
 
 
           console.log(distanceBetweenx +" " + distanceBetweeny)
         }
         d3.select(".high > span").text(gameStats.bestScore)
         d3.select(".current > span").text(gameStats.score)
+        d3.select(".collisions > span").text(gameStats.collisions)
 
 
       // console.log(enemy.attr('cx'));
     };
-  }).attr('cx', function(){
+  }).attr('x', function(){
       return axez.x(Math.random()*100)
     })
-    .attr('cy', function(){
+    .attr('y', function(){
       return axez.y(Math.random()*100)
     })
 
   // d3.transition().
 
   enemies.enter()
-    .append('svg:circle')
-    .attr('class','enemy')
-    .attr('cx', function(enemy){
+    .append('svg:image')
+    // .attr('class','enemy')
+    .attr('xlink:href', "images/cloud.svg")
+    .attr('class', 'enemy')
+    .attr('width', '70')
+    .attr('height', '70')
+
+    .attr('x', function(enemy){
 
       return axez.x(enemy.x)
 
-    }).attr('cy',function(enemy){
+    }).attr('y',function(enemy){
       return axez.y(enemy.y)
-    }).attr('r',10);
+    })
+    .attr('r',80);
 
   // enemies.exit().remove();
 
@@ -134,8 +148,8 @@ checkCollision = function(enemy, collidedCallback) {
   return function(player){
   var radiusSum, separation, xDiff, yDiff;
   radiusSum = parseFloat(enemy.attr('r')) + player.r;
-  xDiff = parseFloat(enemy.attr('cx')) - player.x;
-  yDiff = parseFloat(enemy.attr('cy')) - player.y;
+  xDiff = parseFloat(enemy.attr('x')) - player.x;
+  yDiff = parseFloat(enemy.attr('y')) - player.y;
   separation = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
   if (separation < radiusSum) {
     return collidedCallback(player, enemy);
